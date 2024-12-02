23ddfe539d366cc1cb5bf52cf6ec2eb557696313
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-12-02 09:27:08 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,7 @@ import TransformingForeign from "./Components/BlogPage/BlogInnerPages/Transformi
 import TopTenReasons from "./Components/BlogPage/BlogInnerPages/TopTenReasons/TopTenReasons";
 import SeoServiceTours from "./Components/BlogPage/BlogInnerPages/SeoServiceTours/SeoServiceTours";
 import SeoAttestationBoost from "./Components/BlogPage/BlogInnerPages/SeoAttestationBoost/SeoAttestationBoost";
+import SnowFall from "./Components/Christmas/SnowFall/SnowFall";
 function MainApp() {
   const location = useLocation();
 
@@ -52,6 +53,7 @@ function MainApp() {
     <div className="App">
       {!hideNavbarPaths.includes(location.pathname) && <NavBar />}
       {!hideNavbarPaths.includes(location.pathname) && <ScrollTo />}
+      <SnowFall/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/contact-us" element={<ContactUs />} />
