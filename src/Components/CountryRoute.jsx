4ebe78e9dbe806b5e4cdc0a76db47527ef86b193
Prajwal H S <prajwalhs4india@gmail.com>
2025-09-24 17:22:11 +0530

    country test

---
+++ src/Components/CountryRoute.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useParams, Navigate } from "react-router-dom";
+
+function CountryRoute({ allowedCountries, children }) {
+  const { countryCode } = useParams();
+
+  // If current country is NOT allowed, redirect to home or any fallback
+  if (!allowedCountries.includes(countryCode)) {
+    return <Navigate to={`/${countryCode || ""}`} replace />;
+  }
+
+  return children;
+}
+
+export default CountryRoute;
