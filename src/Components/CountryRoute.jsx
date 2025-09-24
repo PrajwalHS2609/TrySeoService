c14bfc847b88cbf940dfaaee70ec4a51a209be98
Prajwal H S <prajwalhs4india@gmail.com>
2025-09-24 17:29:49 +0530

    country rev

--- src/Components/CountryRoute.jsx
+++ src/Components/CountryRoute.jsx
@@ -1,15 +1,26 @@
-import React from "react";
-import { useParams, Navigate } from "react-router-dom";
+// import React from "react";
+// import { useParams, Navigate } from "react-router-dom";
 
-function CountryRoute({ allowedCountries, children }) {
-  const { countryCode } = useParams();
+// function CountryRoute({ allowedCountries, children }) {
+//   const { countryCode } = useParams();
 
-  // If current country is NOT allowed, redirect to home or any fallback
-  if (!allowedCountries.includes(countryCode)) {
-    return <Navigate to={`/${countryCode || ""}`} replace />;
-  }
+//   // If current country is NOT allowed, redirect to home or any fallback
+//   if (!allowedCountries.includes(countryCode)) {
+//     return <Navigate to={`/${countryCode || ""}`} replace />;
+//   }
 
-  return children;
+//   return children;
+// }
+
+// export default CountryRoute;
+import React from 'react'
+
+const CountryRoute = () => {
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
-export default CountryRoute;
+export default CountryRoute
